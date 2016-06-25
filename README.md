a3547ef24b26a2a61327a7fe7943ed822f071875
tilhom <ilhomteshaboyev@gmail.com>
2016-06-25 11:50:22 +0500

    the first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Sabily - a project of the game for self-improvement
+================================
+
+The game consists of two stages:
+1. Hammering nails
+2. Removing the nails
+
+User hammers nails n the following cases:
+1. If there is anger, impatience
+2. lied, cheated
+3. when not helping, caring
+
+When the user reaches that day does not hammer a single nail,the programm removes 
+the nail of the user statistics
---
+++ frontend/config/main.php
@@ -0,0 +1,50 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'name' => 'Sabily',
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'view' => [
+    'class' => 'yii\web\View',
+    'theme' => [
+      'class' => 'yii\base\Theme',
+      'pathMap' => ['@frontend/views' => '@frontend/themes/material-default'],
+      'baseUrl'   => '@web/themes/material-default'
+    ]
+]
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'params' => $params,
+];
